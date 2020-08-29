046f5b8c9ef1d537834757c94c23203c355bc5e0
YYxxxiiii <axii_@outlook.com>
2020-08-29 19:50:08 +0800

    101.对称二叉树

---
+++ IsSymmetric.java
@@ -0,0 +1,47 @@
+package interviewTree;
+
+
+/*
+*
+* 101. 对称二叉树
+给定一个二叉树，检查它是否是镜像对称的。
+
+
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+
+
+但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+
+    1
+   / \
+  2   2
+   \   \
+   3    3*/
+public class IsSymmetric {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left,root.right);
+    }
+
+    private boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+        if (t1.val != t2.val) {
+            return false;
+        }
+        return isMirror(t1.left,t2.right) && isMirror(t1.right,t2.left);
+    }
+}
